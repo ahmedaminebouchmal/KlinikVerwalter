936940f009d541aca3672daf8f40dedbf97a715f
ahmedaminebouchmal <bouchmalaabesp@gmail.com>
2024-08-24 13:51:44 +0200

    final api config

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,13 +1,10 @@
-import { NextResponse } from "next/server";
-
 export const dynamic = "force-dynamic";
 
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  // This line will throw an error and stop execution
+  // Throwing an error to test Sentry's error monitoring
   throw new Error("Sentry Example API Route Error");
 
-  // This line is unreachable and will never be executed
-  // To fix the issue, you should remove or comment out unreachable code
+  // This line is unreachable and can be removed
   // return NextResponse.json({ data: "Testing Sentry Error..." });
 }
